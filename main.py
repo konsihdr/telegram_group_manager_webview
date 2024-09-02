57378ac5c9fbff6741f750751f84dad1d3550bef
Konstantin <konsi@hdr-it.de>
2024-09-02 22:14:08 +0200

    better sql query

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 from sql import engine, Groups
 from sqlalchemy.orm import Session
-from sqlalchemy import desc
+from sqlalchemy import and_
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     with Session(engine) as s:
-        groups = s.query(Groups).order_by(desc(Groups.group_name)).all()
+        groups = s.query(Groups).filter(and_(Groups.group_active, Groups.group_invite_link != "")).order_by(Groups.group_name).all()
     return render_template('index.html', groups=groups)
 
 if __name__ == '__main__':
